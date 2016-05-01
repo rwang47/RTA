57e9094f4846c4ffe84fa3e36292577d65072d9b
Rou <rwang47@illinois.edu>
2016-04-30 19:35:36 -0500

    add createURL

---
+++ app/src/main/java/com/example/rou/rta/FindPath.java
@@ -0,0 +1,45 @@
+package com.example.rou.rta;
+
+import android.os.AsyncTask;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Rou on 4/30/16.
+ */
+
+public class FindPath {
+    private static final String DIRECTION_URL_API = "https://maps.googleapis.com/maps/api/directions/json?";
+    private static final String GOOGLE_API_KEY = "AIzaSyCyPvmYekzPXT7kQkcb0sjuQpWpKerYFTs";
+    private FindPathListener listener;
+    private String origin;
+    private String destination;
+
+    public FindPath(FindPathListener listener, String origin, String destination) {
+        this.listener = listener;
+        this.origin = origin;
+        this.destination = destination;
+    }
+
+    private String createUrl() throws UnsupportedEncodingException {
+        String urlOrigin = URLEncoder.encode(origin, "utf-8");
+        String urlDestination = URLEncoder.encode(destination, "utf-8");
+
+        return DIRECTION_URL_API + "origin=" + urlOrigin + "&destination=" + urlDestination + "&key=" + GOOGLE_API_KEY;
+    }
+}
